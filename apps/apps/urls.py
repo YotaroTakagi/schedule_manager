9cea0900fabe169a8ba00e439ace6e1d821b18fc
YotaroTakagi <raforemon@gmail.com>
2021-04-05 11:40:43 +0900

    for pythonanywhere

--- apps/apps/urls.py
+++ apps/apps/urls.py
@@ -4,5 +4,6 @@ from django.urls import include, path
 urlpatterns = [
     #includeは他のURLconfへの参照が可能。
     path("schedule/", include("schedule.urls")),
+    path("", include("schedule.urls")),
     path('admin/', admin.site.urls),
 ]
